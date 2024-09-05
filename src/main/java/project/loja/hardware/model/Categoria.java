78c63b585efbdf638a92e9794249153e184b300b
LuizBeraldi <luizhenrique.beraldi@gmail.com>
2024-09-04 22:07:51 -0300

    feat: :sparkles: Implementando CategoriaModel

---
+++ src/main/java/project/loja/hardware/model/Categoria.java
@@ -0,0 +1,23 @@
+package project.loja.hardware.model;
+
+public class Categoria {
+    private int idCategoria;
+    private String nome;
+    
+
+    public int getIdCategoria() {
+        return this.idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
