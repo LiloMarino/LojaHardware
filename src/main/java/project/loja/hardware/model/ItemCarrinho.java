806cac82b5c0a3f82cd5e5a053bf352e515a52a2
Murilo <lilo.marino@hotmail.com>
2024-09-05 17:39:10 -0300

    feat: :sparkles: Implementado ItemCarrinhoRepository
    Closes #19

---
+++ src/main/java/project/loja/hardware/model/ItemCarrinho.java
@@ -0,0 +1,31 @@
+package project.loja.hardware.model;
+
+public class ItemCarrinho {
+    private int idCliente;
+    private int idProduto;
+    private int quantidade;
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
