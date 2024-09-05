28b01d0e7909b31619d3a4e37d93787df4361d00
Murilo <lilo.marino@hotmail.com>
2024-09-05 16:23:06 -0300

    feat: :sparkles: Implementado ProdutoService
    Closes #15

---
+++ src/main/java/project/loja/hardware/service/ProdutoService.java
@@ -0,0 +1,37 @@
+package project.loja.hardware.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import project.loja.hardware.model.Produto;
+import project.loja.hardware.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public void cadastrarProduto(Produto produto) {
+        produtoRepository.save(produto);
+    }
+
+    public Produto getProdutoById(int id) {
+        return produtoRepository.findById(id);
+    }
+
+    public List<Produto> listarProdutos() {
+        return produtoRepository.findAll();
+    }
+
+    public void atualizarProduto(int id, Produto produtoAtualizado) {
+        produtoRepository.update(id, produtoAtualizado);
+    }
+
+    public void deletarProduto(int id) {
+        produtoRepository.deleteById(id);
+    }
+
+}
