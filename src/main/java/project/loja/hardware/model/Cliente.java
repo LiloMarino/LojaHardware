cfe27e93c3ea2e50868a9b51ff393f8bf288fc02
LuizBeraldi <luizhenrique.beraldi@gmail.com>
2024-09-04 22:06:56 -0300

    feat: :sparkles: Implementando ClienteModel

--- src/main/java/project/loja/hardware/model/Cliente.java
+++ src/main/java/project/loja/hardware/model/Cliente.java
@@ -1,5 +1,49 @@
 package project.loja.hardware.model;
 
 public class Cliente {
-    
+    private int idCliente;
+    private String cpf;
+    private String senha;
+    private String email;
+    private String nome;
+
+    public int getIdCliente() {
+        return this.idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return this.senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
